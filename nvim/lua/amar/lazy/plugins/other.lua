05bf086e2e0f87562f6ba29d169b0ed343bcfcbb
Amar Sahinovic <amarsahinovic@gmail.com>
2024-09-20 09:46:56 +0200

    Updates and refactorings

---
+++ nvim/lua/amar/lazy/plugins/other.lua
@@ -0,0 +1,93 @@
+-- Other plugins that do not have their own file (yet)
+return {
+
+  { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
+
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  -- Here is a more advanced example where we pass configuration
+  -- options to `gitsigns.nvim`. This is equivalent to the following lua:
+  --    require('gitsigns').setup({ ... })
+  --
+  -- See `:help gitsigns` to understand what the configuration keys do
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+
+  { -- Useful plugin to show you pending keybinds.
+    'folke/which-key.nvim',
+    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    config = function() -- This is the function that runs, AFTER loading
+      require('which-key').setup()
+
+      -- Document existing key chains
+      require('which-key').add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+      }
+    end,
+  },
+
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+  -- {
+  --   'akinsho/bufferline.nvim',
+  --   dependencies = 'nvim-tree/nvim-web-devicons',
+  --   opts = {
+  --     options = {
+  --       -- separator_style = 'slope',
+  --       hover = {
+  --         enabled = true,
+  --         delay = 200,
+  --         reveal = { 'close' },
+  --       },
+  --     },
+  --   },
+  -- },
+  {
+    'Bekaboo/dropbar.nvim',
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      'nvim-telescope/telescope-fzf-native.nvim',
+    },
+    config = true,
+  },
+  {
+    'otavioschwanck/arrow.nvim',
+    opts = {
+      show_icons = true,
+      leader_key = ';', -- Recommended to be a single key
+    },
+    config = true,
+  },
+
+  {
+    'dstein64/nvim-scrollview',
+    config = function(_, opts)
+      require('scrollview').setup(opts)
+      require('scrollview.contrib.gitsigns').setup(opts)
+    end,
+    opts = {
+      always_show = true,
+      -- column = 40,
+    },
+  },
+}
